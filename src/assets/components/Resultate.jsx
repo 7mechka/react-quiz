e6ae541b5b6326daea6f83273a9b52cbe621976c
Maksym Stognij <7mechka@gmail.com>
2024-03-10 19:59:10 +0200

    tipical commit

--- src/assets/components/Resultate.jsx
+++ src/assets/components/Resultate.jsx
@@ -4,17 +4,9 @@ function Resultate({ results, isShow }) {
   return (
     <section className={`quiz__result ${isShow}`}>
       <h3 className="quiz__summary">
-        Ви відповіли правильно на {results} із {Store.quiz.questions.length}{" "}
-        запитань
+        Ви відповіли правильно на {results} із {Store.quiz.questions.length} запитань
       </h3>
-      <button
-        className="quiz__selector"
-        onClick={() => {
-          location.reload();
-        }}
-      >
-        Пройти ще раз
-      </button>
+      <button className="quiz__selector" onClick={() => {location.reload()}}>Пройти ще раз</button>
     </section>
   );
 }
